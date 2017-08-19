f6c329d8a460e49cd6ac8de5ec82d250b5fea364
Christine516 <you@example.com>
2017-08-19 02:26:54 -0700

    please please work

--- wordlyAsia.js
+++ wordlyAsia.js
@@ -399,7 +399,7 @@ function chile(){
 	 function algeriaInfoClick(){
 	 	console.log(document.getElementById("algeriaInfo"))
 	    document.getElementById("algeriaInfo").style.display = 'inline'; 
-	      	document.getElementById("ghanaComment").style.display = 'none';
+	      	document.getElementById("algeriaqComment").style.display = 'none';
 
 	  }
 	  function algeriaCommentClick(){
@@ -407,18 +407,218 @@ function chile(){
 
 	  	document.getElementById("algeriaComment").style.display = 'inline';
 	  }
+//USA
+		function usa(){
+	 	    document.getElementById("usaInfo").style.display = 'inline'; 
+	      	document.getElementById("usaComment").style.display = 'none';
 
+	 }
+	 function usaInfoClick(){
+	 	console.log(document.getElementById("usaInfo"))
+	    document.getElementById("usaInfo").style.display = 'inline'; 
+	      	document.getElementById("usaComment").style.display = 'none';
 
+	  }
+	  function usaCommentClick(){
+	  	    document.getElementById("usaInfo").style.display = 'none'; 
 
+	  	document.getElementById("usaComment").style.display = 'inline';
+	  }
 
+//MEXICO
+		function mexico(){
+	 	    document.getElementById("mexicoInfo").style.display = 'inline'; 
+	      	document.getElementById("mexicoComment").style.display = 'none';
 
+	 }
+	 function mexicoInfoClick(){
+	 	console.log(document.getElementById("mexicoInfo"))
+	    document.getElementById("mexicoInfo").style.display = 'inline'; 
+	      	document.getElementById("mexicoComment").style.display = 'none';
 
+	  }
+	  function mexicoCommentClick(){
+	  	    document.getElementById("mexicoInfo").style.display = 'none'; 
 
+	  	document.getElementById("mexicoComment").style.display = 'inline';
+	  }
 
+//CANADA
+		function canada(){
+	 	    document.getElementById("canadaInfo").style.display = 'inline'; 
+	      	document.getElementById("canadaComment").style.display = 'none';
 
+	 }
+	 function canadaInfoClick(){
+	 	console.log(document.getElementById("canadaInfo"))
+	    document.getElementById("canadaInfo").style.display = 'inline'; 
+	      	document.getElementById("canadaComment").style.display = 'none';
 
+	  }
+	  function canadaCommentClick(){
+	  	    document.getElementById("canadaInfo").style.display = 'none'; 
 
+	  	document.getElementById("canadaComment").style.display = 'inline';
+	  }
 
+//EGYPT	
+		function egypt(){
+	 	    document.getElementById("egyptInfo").style.display = 'inline'; 
+	      	document.getElementById("egyptComment").style.display = 'none';
 
+	 }
+	 function egyptInfoClick(){
+	 	console.log(document.getElementById("egyptInfo"))
+	    document.getElementById("egyptInfo").style.display = 'inline'; 
+	      	document.getElementById("egyptComment").style.display = 'none';
 
+	  }
+	  function egyptCommentClick(){
+	  	    document.getElementById("egyptInfo").style.display = 'none'; 
+
+	  	document.getElementById("egyptComment").style.display = 'inline';
+	  }
+
+//SAUDO ARABIA
+		function sa(){
+	 	    document.getElementById("saInfo").style.display = 'inline'; 
+	      	document.getElementById("saComment").style.display = 'none';
+
+	 }
+	 function saInfoClick(){
+	 	console.log(document.getElementById("saInfo"))
+	    document.getElementById("saInfo").style.display = 'inline'; 
+	      	document.getElementById("saComment").style.display = 'none';
+
+	  }
+	  function saCommentClick(){
+	  	    document.getElementById("saInfo").style.display = 'none'; 
+
+	  	document.getElementById("saComment").style.display = 'inline';
+	  }
+
+//UNITED ARAB EMIRATES
+		function uae(){
+	 	    document.getElementById("uaeInfo").style.display = 'inline'; 
+	      	document.getElementById("uaeComment").style.display = 'none';
+
+	 }
+	 function uaeInfoClick(){
+	 	console.log(document.getElementById("uaeInfo"))
+	    document.getElementById("uaeInfo").style.display = 'inline'; 
+	      	document.getElementById("uaeComment").style.display = 'none';
+
+	  }
+	  function uaeCommentClick(){
+	  	    document.getElementById("uaeInfo").style.display = 'none'; 
+
+	  	document.getElementById("uaeComment").style.display = 'inline';
+	  }
+
+//TURKEY
+		function turkey(){
+	 	    document.getElementById("turkeyInfo").style.display = 'inline'; 
+	      	document.getElementById("turkeyComment").style.display = 'none';
+
+	 }
+	 function turkeyInfoClick(){
+	 	console.log(document.getElementById("turkeyInfo"))
+	    document.getElementById("turkeyInfo").style.display = 'inline'; 
+	      	document.getElementById("turkeyComment").style.display = 'none';
+
+	  }
+	  function turkeyCommentClick(){
+	  	    document.getElementById("turkeyInfo").style.display = 'none'; 
+
+	  	document.getElementById("turkeyComment").style.display = 'inline';
+	  }
+
+//AUSTRALIA
+		function australia(){
+	 	    document.getElementById("australiaInfo").style.display = 'inline'; 
+	      	document.getElementById("australiaComment").style.display = 'none';
+
+	 }
+	 function australiaInfoClick(){
+	 	console.log(document.getElementById("australiaInfo"))
+	    document.getElementById("australiaInfo").style.display = 'inline'; 
+	      	document.getElementById("australiaComment").style.display = 'none';
+
+	  }
+	  function australiaCommentClick(){
+	  	    document.getElementById("australiaInfo").style.display = 'none'; 
+
+	  	document.getElementById("australiaComment").style.display = 'inline';
+	  }
+//NEW ZEALAND
+		function nz(){
+	 	    document.getElementById("nzInfo").style.display = 'inline'; 
+	      	document.getElementById("nzComment").style.display = 'none';
+
+	 }
+	 function nzInfoClick(){
+	 	console.log(document.getElementById("nzInfo"))
+	    document.getElementById("nzInfo").style.display = 'inline'; 
+	      	document.getElementById("nzComment").style.display = 'none';
+
+	  }
+	  function nzCommentClick(){
+	  	    document.getElementById("nzInfo").style.display = 'none'; 
+
+	  	document.getElementById("nzComment").style.display = 'inline';
+	  }
+
+//NEW GUINEA
+		function ng(){
+	 	    document.getElementById("ngInfo").style.display = 'inline'; 
+	      	document.getElementById("ngComment").style.display = 'none';
+
+	 }
+	 function ngInfoClick(){
+	 	console.log(document.getElementById("ngInfo"))
+	    document.getElementById("ngInfo").style.display = 'inline'; 
+	      	document.getElementById("ngComment").style.display = 'none';
+
+	  }
+	  function ngCommentClick(){
+	  	    document.getElementById("ngInfo").style.display = 'none'; 
+
+	  	document.getElementById("ngComment").style.display = 'inline';
+	  }
+
+//TASMANIA
+		function tas(){
+	 	    document.getElementById("tasInfo").style.display = 'inline'; 
+	      	document.getElementById("tasComment").style.display = 'none';
+
+	 }
+	 function tasInfoClick(){
+	 	console.log(document.getElementById("tasInfo"))
+	    document.getElementById("tasInfo").style.display = 'inline'; 
+	      	document.getElementById("tasComment").style.display = 'none';
+
+	  }
+	  function tasCommentClick(){
+	  	    document.getElementById("tasInfo").style.display = 'none'; 
+
+	  	document.getElementById("tasComment").style.display = 'inline';
+	  }
+
+//FIJI
+		function fiji(){
+	 	    document.getElementById("fijiInfo").style.display = 'inline'; 
+	      	document.getElementById("fijiComment").style.display = 'none';
+
+	 }
+	 function fijiInfoClick(){
+	 	console.log(document.getElementById("fijiInfo"))
+	    document.getElementById("fijiInfo").style.display = 'inline'; 
+	      	document.getElementById("fijiComment").style.display = 'none';
+
+	  }
+	  function fijiCommentClick(){
+	  	    document.getElementById("fijiInfo").style.display = 'none'; 
+
+	  	document.getElementById("fijiComment").style.display = 'inline';
+	  }
 
